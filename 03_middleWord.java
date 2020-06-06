7b00d0649b5f4c8fbe6a7c43fc5afc832503e27a
Behrouz-atoofi <40962954+Behrouz-atoofi@users.noreply.github.com>
2020-06-07 01:41:22 +0430

    Create 03_middleWord.java

---
+++ 03_middleWord.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter the sentence :");
+        String sentence = sc.nextLine() ;
+        String [] words = sentence.split(" ") ;
+        ArrayList<String> strings = new ArrayList<>() ;
+        for ( String word : words) {
+            strings.add(word) ;
+        }
+        System.out.println(strings.get(strings.size()/2));
+
+
+
+    }
+}
