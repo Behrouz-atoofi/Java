a5d0c6b8bcdc67a1b4816253bbbb1d2dc1ca1505
Behrouz-atoofi <40962954+Behrouz-atoofi@users.noreply.github.com>
2020-06-07 01:43:01 +0430

    Create 06_1001sharecode.java

---
+++ 06_1001sharecode.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in) ;
+        int a,b,sum = 0 ;
+
+        while(true) {
+        a = scanner.nextInt() ;
+        b = scanner.nextInt() ;
+        sum = a+b ;
+        System.out.println(sum);
+
+    } } }
+
